f38e2b200cc830a2e0a3a14c82cc4eb1281e3642
Bharath <e.bharath@nokia.com>
2020-10-25 22:27:21 +0530

    notification fix

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -25,31 +25,48 @@ window.addEventListener('beforeinstallprompt', function(event) {
 });
 
 function displayConfirmNotification() {
-  console.log('displayConfirmNotification')
-  if ('serviceWorker' in navigator) {
-    console.log('test')
-    var options = {
-      body: 'You successfully subscribed to our Notification service!',
-      icon: '/src/images/icons/app-icon-96x96.png',
-      image: '/src/images/main-image.jpg',
-      dir: 'ltr',
-      lang: 'en-US', // BCP 47,
-      vibrate: [100, 50, 200],
-      badge: '/src/images/icons/app-icon-96x96.png',
-      tag: 'confirm-notification',
-      renotify: true,
-      actions: [
-        { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
-        { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
-      ]
-    };
-
-    navigator.serviceWorker.ready
-      .then(function(swreg) {
-        swreg.showNotification('Successfully subscribed (from SW)!', options);
-      });
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      var options = {
+        body: 'Here is a notification body!',
+        icon: '/src/images/icons/app-icon-96x96.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        }
+      };
+      reg.showNotification('Hello world!', options);
+    });
   }
 }
+
+// function displayConfirmNotification() {
+//   console.log('displayConfirmNotification')
+//   if ('serviceWorker' in navigator) {
+//     console.log('test')
+//     var options = {
+//       body: 'You successfully subscribed to our Notification service!',
+//       icon: '/src/images/icons/app-icon-96x96.png',
+//       image: '/src/images/main-image.jpg',
+//       dir: 'ltr',
+//       lang: 'en-US', // BCP 47,
+//       vibrate: [100, 50, 200],
+//       badge: '/src/images/icons/app-icon-96x96.png',
+//       tag: 'confirm-notification',
+//       renotify: true,
+//       actions: [
+//         { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
+//         { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
+//       ]
+//     };
+//
+//     navigator.serviceWorker.ready
+//       .then(function(swreg) {
+//         swreg.showNotification('Successfully subscribed (from SW)!', options);
+//       });
+//   }
+// }
 
 function askForNotificationPermission() {
   Notification.requestPermission().then(function(result){
